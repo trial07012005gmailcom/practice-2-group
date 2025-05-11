73e21af4b005f01f4502afa01b49d13449808813
marianavillarroel <marianavillarroel1@upb.edu>
2025-05-11 15:31:33 -0400

    P3-002 Added new method AssignGiftToPatient on GiftManager.cs

--- PatientManager/Managers/GiftManager.cs
+++ PatientManager/Managers/GiftManager.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Services.Models;
 using Services.ExternalServices;
+using PatientManager.Services;
 
 namespace PatientManager.Managers
 {
@@ -21,5 +22,20 @@ namespace PatientManager.Managers
         {
             return await _store.GetAllElectronicItems();
         }
+
+        public async Task<Electronic?> AssignGiftToPatient(string ci, PatientService patientService)
+        {
+            var patient = patientService.GetPatientByCi(ci);
+            if (patient == null)
+                return null;
+
+            var gifts = await GetGiftsAsync();
+            if (gifts.Count == 0)
+                return null;
+
+            var rnd = new Random();
+            return gifts[rnd.Next(gifts.Count)];
+        }
+
     }
 }
