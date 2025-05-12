38601b93a222044c41cee0e65afc518ecc3934fb
majoAchabal <mariaachabal1@upb.edu>
2025-05-12 14:47:47 -0400

    P3-002: Integracion completa con configuracion modificada

--- Services/ExternalServices/PatientCodeService.cs
+++ Services/ExternalServices/PatientCodeService.cs
@@ -3,23 +3,24 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Services.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Services.ExternalServices
 {
     public class PatientCodeService
     {
         private readonly HttpClient _httpClient;
+        private readonly string _baseUrl;
         private readonly string[] _bloodTypes = { "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-" };
 
-        public PatientCodeService(HttpClient httpClient)
+        public PatientCodeService(HttpClient httpClient, IConfiguration config)
         {
             _httpClient = httpClient;
+            _baseUrl = config["PatientCodeService:BaseUrl"];
         }
 
         public async Task<string> GetPatientCodeAsync(string name, string lastName, string ci)
         {
-            string url = "https://practice-3-hqeuemhvd2cwevhp.centralus-01.azurewebsites.net/api/patientcode/generate";
-
             var random = new Random();
             var bloodType = _bloodTypes[random.Next(_bloodTypes.Length)];
 
@@ -33,6 +34,8 @@ namespace Services.ExternalServices
 
             var json = JsonConvert.SerializeObject(body);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = $"{_baseUrl}/api/patientcode/generate";
 
             var response = await _httpClient.PostAsync(url, content);
             response.EnsureSuccessStatusCode();
